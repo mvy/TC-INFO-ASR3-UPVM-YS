6c34466107cc8e5f8225610857df36b4e9be25ca
Yves Stadler <mvyonline@gmail.com>
2011-09-09 23:43:59 +0200

    Directory creation - cmake initialisation

---
+++ CMakeFiles/ps.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+FILE(REMOVE_RECURSE
+  "build//presentation.bbl"
+  "build//presentation.blg"
+  "build//presentation-blx.bib"
+  "build//presentation.glg"
+  "build//presentation.glo"
+  "build//presentation.gls"
+  "build//presentation.idx"
+  "build//presentation.ilg"
+  "build//presentation.ind"
+  "build//presentation.ist"
+  "build//presentation.log"
+  "build//presentation.out"
+  "build//presentation.toc"
+  "build//presentation.lof"
+  "build//presentation.xdy"
+  "build//presentation.synctex.gz"
+  "build//presentation.synctex.bak.gz"
+  "build//presentation.dvi"
+  "build//presentation.ps"
+  "build//presentation.pdf"
+  "build//CH02-IntroductionUnix/presentation.aux"
+  "build//CH02-IntroductionUnix/presentation.tex.aux"
+  "build//CH02-IntroductionUnix/introductionyves.aux"
+  "build//CH02-IntroductionUnix/introductionyves.tex.aux"
+  "CMakeFiles/ps"
+  "build//presentation.ps"
+  "build//presentation.dvi"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
